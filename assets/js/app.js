bf8564a3bfecd2d4e8dedf46a31a1a72d7ce7ec3
Charlotte Stone <charlie.stone649@gmail.com>
2023-10-11 15:19:12 +0000

    refined the subtraction and division question functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -111,8 +111,8 @@ function displayAdditionQuestion(operand1, operand2) {
  * For displaying the operands for subtraction questions
  */
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1
-    document.getElementById("operand2").textContent = operand2
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2
+    document.getElementById("operand2").textContent = operand2 > operand1 ? operand1 : operand2
     document.getElementById("operator").textContent = "-"
 }
 
@@ -129,8 +129,8 @@ function displayMultiplyQuestion(operand1, operand2) {
  * For displaying the operands for division questions
  */
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1
-    document.getElementById("operand2").textContent = operand2
+    document.getElementById("operand1").textContent =  operand1 > operand2 ? operand1 : operand2
+    document.getElementById("operand2").textContent = operand2 > operand1 ? operand1 : operand2
     document.getElementById("operator").textContent = "/"
 }
 
