3ee18c88053db8a8228733d88f72663fbe2c7a5a
Charlotte Stone <charlie.stone649@gmail.com>
2023-10-11 13:59:48 +0000

    Added two random number variables for use in the runGame function.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button")
+
+    for(let button of buttons){
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit!")
+            } else {
+                let gameType = this.getAttribute("data-type")
+                alert(`You clicked ${gameType}`)
+            }
+        })
+    }
+})
+
+
+/**
+ * The main game "loop",called when the script is first loaded 
+ * and after the user's answer has been processed.
+ */
 function runGame(){
 
+    // Create the two random numbers to be used for the game.
+    let num1 = Math.floor(Math.random() * 25) + 1
+    let num2 = Math.floor(Math.random() * 25) + 1
 }
 
 function checkAnswer(){
@@ -33,3 +56,4 @@ function displayMultiplyQuestion() {
 function displayDivisionQuestion() {
     
 }
+
